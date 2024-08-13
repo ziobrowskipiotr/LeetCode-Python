0a3301ac1fa54342d94cdc2bd67cb3ad3c407810
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-08-13 18:01:41 +0200

    Time: 55 ms (55.91%), Space: 16.6 MB (70.3%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -7,51 +7,34 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         list_head = ListNode()
         list_node = list_head
-        prev1 = l1
-        curr1 = l1.next
-        prev2 = l2
-        curr2 = l2.next
-        while curr1 is not None:
-            next1 = curr1.next
-            curr1.next = prev1
-            l1.next = next1
-            prev1, curr1 = curr1, next1
-        while curr2 is not None:
-            next2 = curr2.next
-            curr2.next = prev2
-            l2.next = next2
-            prev2, curr2 = curr2, next2
-        while prev1 is not None and prev2 is not None:
+
+        while l1 is not None and l2 is not None:
             list_node.next = ListNode()
-            if prev1.val + prev2.val >= 10:
-                list_node.next.val = prev1.val + prev2.val - 10
-                if prev1.next is None:
-                    prev1.next = ListNode()
-                prev1.next.val += 1
+            if l1.val + l2.val >= 10:
+                if l1.next is None:
+                    l1.next = ListNode()
+                list_node.next.val = l1.val + l2.val - 10
+                l1.next.val += 1
             else:
-                list_node.next.val = prev1.val + prev2.val
-            prev1, prev2, list_node = prev1.next, prev2.next, list_node.next
-        if prev1 is None and prev2 is None:
+                list_node.next.val = l1.val + l2.val
+            list_node, l1, l2 = list_node.next, l1.next, l2.next
+
+        if l1 is None and l2 is None:
             return list_head.next
-        elif prev1 is None:
-            while prev2 is not None:
-                if prev2.val == 10:
-                    if prev2.next is None:
-                        prev2.next = ListNode()
-                    prev2.next.val += 1
-                    prev2.val = 0
-                list_node.next = prev2
-                list_node, prev2 = list_node.next, prev2.next
+
+        if l1 is None:
+            list_node.next = l2
             return list_head.next
-        elif prev2 is None:
-            while prev1 is not None:
-                if prev1.val == 10:
-                    if prev1.next is None:
-                        prev1.next = ListNode()
-                    prev1.next.val += 1
-                    prev1.val = 0
-                list_node.next = prev1
-                list_node, prev1 = list_node.next, prev1.next
+            
+        if l2 is None:
+            while l1 is not None:
+                if l1.val == 10:
+                    if l1.next is None:
+                        l1.next = ListNode()
+                    l1.next.val += 1
+                    l1.val = 0
+                list_node.next = l1
+                list_node, l1 = list_node.next, l1.next
             return list_head.next
 
 
