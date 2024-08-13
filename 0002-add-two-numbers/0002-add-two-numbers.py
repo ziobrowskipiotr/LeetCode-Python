d00c297f0e1826edadf6ed82b7cd6bd01452e86f
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-08-13 18:08:42 +0200

    Time: 59 ms (32.28%), Space: 16.5 MB (70.3%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -8,7 +8,6 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         list_head = ListNode()
         list_node = list_head
-
         while l1 is not None and l2 is not None:
             list_node.next = ListNode()
             if l1.val + l2.val >= 10:
@@ -19,14 +18,11 @@ class Solution:
             else:
                 list_node.next.val = l1.val + l2.val
             list_node, l1, l2 = list_node.next, l1.next, l2.next
-
         if l1 is None and l2 is None:
             return list_head.next
-
         if l1 is None:
             list_node.next = l2
             return list_head.next
-            
         if l2 is None:
             while l1 is not None:
                 if l1.val == 10:
@@ -37,8 +33,3 @@ class Solution:
                 list_node.next = l1
                 list_node, l1 = list_node.next, l1.next
             return list_head.next
-
-
-        
-        
-        
