4b79301a0568f5c0dc9694426a17a68bd552b9b0
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-08-16 14:03:37 +0200

    Time: 37 ms (74.29%), Space: 16.4 MB (82.89%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
@@ -1,4 +1,3 @@
-# ####
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -6,33 +5,13 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None:
-            return head
-        cur = head
-        nex = head.next
-        prev = None
-        i = False
-
-        while nex is not None:
-            if cur.val == nex.val:
-                nex = cur
-                while cur.val == nex.val:
-                    cur, nex = nex, nex.next
-                    if nex is not None:
-                        cur.next = None
-                    else:
-                        if prev is None:
-                            return None
-                        else:
-                            prev.next = None
-                            return head
-                cur, nex = nex, nex.next
-                if i == False:
-                    head = cur
-                if prev is not None:
-                    prev.next = cur
+        dummy = ListNode(0, head)
+        cur = dummy
+        while cur.next and cur.next.next:
+            if  cur.next.val == cur.next.next.val:
+                v = cur.next.val
+                while cur.next and cur.next.val == v:
+                    cur.next = cur.next.next
             else:
-                prev, cur, nex = cur, cur.next, nex.next
-                i = True
-
-        return head
+                cur = cur.next
+        return dummy.next
