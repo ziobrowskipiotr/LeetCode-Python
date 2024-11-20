41fcbac7b378bf2dbddffd463ed991a2ccc60348
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-11-20 21:42:39 +0100

    Time: 7 ms (62.43%), Space: 16.7 MB (34.01%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,15 +1,13 @@
 class Solution:
-    def isPalindrome(self, x: int) -> bool:
+    def isPalindrome(self, x):
         if x<0:
             return False
         if x<10:
             return True
-        temp_x = x
-        len = 0
-        while temp_x > 0:
-            temp_x = temp_x//10
-            len += 1
-        for i in range(len//2):
-            if (x//pow(10, len-1-i))%10 != (x//pow(10, i))%10:
-                return False
-        return True
+        if x%10 == 0:
+            return False
+        rev_number = 0
+        while x > rev_number:
+            rev_number = rev_number*10 + x%10
+            x//=10
+        return x == rev_number or x == rev_number//10
