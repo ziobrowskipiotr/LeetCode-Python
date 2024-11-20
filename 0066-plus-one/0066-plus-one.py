2c85831fc00e0755396e5452c0a9ee9c69661327
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-11-20 22:10:08 +0100

    Time: 0 ms (100%), Space: 16.7 MB (5.19%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,13 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range (len(digits)-1, -1, -1):
+            digits[i] += 1
+            if digits[i] < 10:
+                return digits
+            else:
+                if i > 0:
+                    digits[i] = 0
+                else:
+                    digits[i] = 0
+                    digits.insert(0,1)
+        return digits
