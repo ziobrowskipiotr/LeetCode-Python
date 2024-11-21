e5c03a114f818967691423d26d36fa1f74c5e7e3
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-11-21 22:38:55 +0100

    Time: 0 ms (100%), Space: 17.1 MB (30.32%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,29 +1,30 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        def rows_search(left, right):
-            if left > right:
-                return -1
+        left = 0
+        right = len(matrix)-1
+        pivot = 0
+        while left <= right:
             pivot = (left+right)//2
             if target <= matrix[pivot][len(matrix[pivot])-1] and target >= matrix[pivot][0]:
-                return pivot
+                break
             else:
                 if target > matrix[pivot][len(matrix[pivot])-1]:
-                    return rows_search(pivot+1, right)
+                    left = pivot+1
                 else:
-                    return rows_search(left, pivot-1)
-        def column_search(left,right, i):
-            if left > right:
-                return False
-            pivot = (left+right)//2
-            if target == matrix[i][pivot]:
+                    right = pivot-1
+        else:
+            return False
+
+        left = 0
+        right = len(matrix[pivot])-1
+        while left <= right:
+            mid = (left+right)//2
+            if target == matrix[pivot][mid]:
                 return True
             else:
-                if target > matrix[i][pivot]:
-                    return column_search(pivot+1, right, i)
+                if target > matrix[pivot][mid]:
+                    left = mid+1
                 else:
-                    return column_search(left, pivot-1, i)
-        row_number = rows_search(0, len(matrix)-1)
-        if row_number == -1:
-            return False
+                    right = mid-1
         else:
-            return column_search(0, len(matrix[row_number])-1, row_number)
+            return False
