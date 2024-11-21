e4677eaa3a03f75524efcf5e22c106a9bf674a00
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-11-21 23:46:52 +0100

    Time: 0 ms (100%), Space: 16.7 MB (44.13%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums)-1
+        while left<right:
+            pivot = (left+right)//2
+            pivot_left = pivot-1
+            pivot_right = pivot+1
+            if pivot_right == len(nums):
+                pivot_right = 0
+            if nums[pivot] > nums[pivot_left] and nums[pivot] > nums[pivot_right]:
+                return pivot
+            else:
+                if nums[pivot] <= nums[pivot_right]:
+                    left = pivot_right
+                elif nums[pivot] <= nums[pivot_left]:
+                    right = pivot_left
+        return left
