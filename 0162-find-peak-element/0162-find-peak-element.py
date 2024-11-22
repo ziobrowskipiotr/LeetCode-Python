0bf49549787e4f0ecde73f814e3dbc70ebd487d2
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-11-22 01:15:26 +0100

    Time: 0 ms (100%), Space: 16.7 MB (78.95%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -3,7 +3,7 @@ class Solution:
         left = 0
         right = len(nums)-1
         while left<right:
-            pivot = (left+right)//2        
+            pivot = (left+right)//2
             if nums[pivot] > nums[pivot-1] and nums[pivot] > nums[pivot+1]:
                 return pivot
             else:
