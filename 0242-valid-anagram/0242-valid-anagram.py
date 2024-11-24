e2890a5002ca6257e95677c42b7e2a037838f658
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-11-25 00:15:55 +0100

    Time: 11 ms (75.52%), Space: 16.8 MB (68.69%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        map_s = defaultdict(int)
+        map_t = defaultdict(int)
+        for letter in s:
+            map_s[letter] += 1
+        for letter in t:
+            if letter not in map_s:
+                return False
+            map_s[letter] -= 1
+        for s_letter in s:
+            if map_s[s_letter] > 0:
+                return False
+        return True
