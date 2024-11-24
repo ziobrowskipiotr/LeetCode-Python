c8e5b25dc842691bd47cb454528f246788143491
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-11-25 00:45:24 +0100

    Time: 11 ms (79.73%), Space: 20.2 MB (51.24%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,8 +2,6 @@ from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         map_strs = defaultdict(list)
-        result = []
         for word in strs:
-            temp = str(sorted(word))
-            map_strs[temp] += [word]
+            map_strs[tuple(sorted(word))] += [word]
         return list(map_strs.values())
