62a4bc26fd4b5bdae5f222abf78cd3c2f79d1575
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-11-24 22:49:23 +0100

    Time: 4 ms (76.56%), Space: 16.7 MB (56.99%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,12 +1,15 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
 
-        hash_map = {}
+        map_s_t = {}
+        map_t_s = {}
         for i in range(len(s)):
-            if s[i] in hash_map:
-                if hash_map[s[i]] != t[i]:
-                    return False
-            elif t[i] in hash_map.values():
+            if s[i] not in map_s_t and t[i] not in map_t_s:
+                map_s_t[s[i]] = t[i]
+                map_t_s[t[i]] = s[i]
+            elif s[i] in map_s_t and map_s_t[s[i]] == t[i]:
+                continue
+            else:
                 return False
-            hash_map[s[i]] = t[i]
+        print(map_s_t, map_t_s)
         return True
