c1595862047278aa64a4586524cf1005bfa146ab
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-11-28 21:50:26 +0100

    Time: 0 ms (100%), Space: 17.3 MB (10.37%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -6,30 +6,17 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        list_head = ListNode()
-        list_node = list_head
-        while l1 is not None and l2 is not None:
-            list_node.next = ListNode()
-            if l1.val + l2.val >= 10:
-                if l1.next is None:
-                    l1.next = ListNode()
-                list_node.next.val = l1.val + l2.val - 10
-                l1.next.val += 1
-            else:
-                list_node.next.val = l1.val + l2.val
-            list_node, l1, l2 = list_node.next, l1.next, l2.next
-        if l1 is None and l2 is None:
-            return list_head.next
-        if l1 is None:
-            list_node.next = l2
-            return list_head.next
-        if l2 is None:
-            while l1 is not None:
-                if l1.val == 10:
-                    if l1.next is None:
-                        l1.next = ListNode()
-                    l1.next.val += 1
-                    l1.val = 0
-                list_node.next = l1
-                list_node, l1 = list_node.next, l1.next
-            return list_head.next
+        current_node = ListNode()
+        first_node = current_node
+        add_number = 0
+        while l1 != None or l2 != None or add_number:
+            cur_val1 = l1.val if l1 != None else 0
+            cur_val2 = l2.val if l2 != None else 0
+            new_node = ListNode((cur_val1 + cur_val2 + add_number)%10)
+            add_number = (cur_val1 + cur_val2 + add_number)//10
+            current_node.next = new_node
+            current_node = new_node
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return first_node.next
