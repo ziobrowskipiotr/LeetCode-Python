487092194540450bbb2b0ba0f2ad89d693438417
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-11-29 00:04:52 +0100

    Time: 0 ms (100%), Space: 17 MB (13.79%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -3,28 +3,23 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-#
+####
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        node_start = ListNode()
-        node = node_start
-        while list1 is not None and list2 is not None:
-            if list1.val < list2.val:
-                node.next = list1
-                list1 = list1.next
-            elif list2.val < list1.val:
-                node.next = list2
+        head = ListNode()
+        current_node = head
+        while list1 != None and list2 != None:
+            if list1.val > list2.val:
+                current_node.next = list2
+                current_node = list2
                 list2 = list2.next
             else:
-                node.next = list1
+                current_node.next = list1
+                current_node = list1
                 list1 = list1.next
-                node = node.next
-                node.next = list2
-                list2 = list2.next
-            node = node.next
-        if list1 is None:
-            node.next = list2
-        elif list2 is None:
-            node.next = list1
-
-        return node_start.next
+        else:
+            if not list1:
+                current_node.next = list2
+            else:
+                current_node.next = list1
+        return head.next
