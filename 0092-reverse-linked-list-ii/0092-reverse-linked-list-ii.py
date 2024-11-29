789d5398c0420e7b7970cc24f10354a1530fef7a
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-11-29 23:59:56 +0100

    Time: 0 ms (100%), Space: 17.3 MB (14.15%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -7,53 +7,29 @@
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
 
-        if left == right:
+        if not head:
             return head
-
-        current_node = head
-        left_node = head
-        right_node = head
-        before_left = head
-        after_right = head
-        prev_node = head
-        ix = 1
         
-        while current_node is not None:
-            if ix == left-1:
-                before_left = current_node
-                current_node = current_node.next
-            elif ix == right+1:
-                after_right = current_node
-                current_node = current_node.next
-            elif ix == left:
-                prev_node = current_node
-                left_node = current_node
+        cur, prev = head, None
 
-                temp_node = current_node
-                current_node = current_node.next
-                temp_node.next = None
-            elif ix > left and ix < right:
-                temp_node = current_node
-                current_node = current_node.next
-                temp_node.next = prev_node
-                prev_node = temp_node
-            elif ix == right:
-                right_node = current_node
-                temp_node = current_node
+        while left > 1:
+            prev = cur
+            cur = cur.next
+            left -= 1
+            right -= 1
 
-                current_node = current_node.next
-                temp_node.next = prev_node
-                prev_node = temp_node
-            else:
-                current_node = current_node.next
-            ix += 1
-
-        if left != 1:
-            before_left.next = right_node
-        if right != ix-1:
-            left_node.next = after_right
-
-        if left == 1:
-            return right_node
-        else:
+        tail, con = cur, prev
+        while right:
+            third = cur.next
+            cur.next = prev
+            prev = cur
+            cur = third
+            right -= 1
+        
+        if con:
+            con.next = prev
+            tail.next = cur
             return head
+        else:
+            tail.next = cur
+            return prev
