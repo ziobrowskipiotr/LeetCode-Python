0008fe7dfe13b18fff4017350fc1bad05b0102f5
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-11-29 18:27:46 +0100

    Time: 0 ms (100%), Space: 17.5 MB (16.2%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -1,20 +1,19 @@
-# ##################################
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        dummy = ListNode(next=head)
-        cur, prev = dummy, dummy
-        while cur.next is not None:
-            if cur.next.val >= x:
-                cur, cur.next = cur.next, cur.next.next
+        if not head:
+            return head
+	
+        left, right = ListNode(), ListNode()
+        ltail, rtail = left, right
+
+        while head:
+            if head.val < x:
+                ltail.next = head
+                ltail = ltail.next
             else:
-                if cur.next != prev.next:
-                    prev.next, cur.next.next, cur.next = cur.next, prev.next, cur.next.next
-                    cur.next, prev = cur.next, prev.next
-                else:
-                    prev, cur, cur.next = prev.next, cur.next, cur.next.next
-        return dummy.next
+                rtail.next = head
+                rtail = rtail.next
+            head = head.next
+        ltail.next = right.next
+        rtail.next = None
+        return left.next
