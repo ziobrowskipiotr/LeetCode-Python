bd8f0a27a17da1e90fe07c6d9ac1500d6453f184
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-11-29 03:13:33 +0100

    Time: 0 ms (100%), Space: 17.4 MB (9.66%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,27 +5,33 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def number_of_nodes(self, head: Optional[ListNode]) -> Optional[int]:
-        h = head
-        i = 0
-        while h is not None:
-            i+=1
-            h = h.next
-        return i
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        num = Solution.number_of_nodes(self, head)
-        if num < 2:
-            return None
+        def number_of_nodes(head):
+            ix = 0
+            while head is not None:
+                ix += 1
+                head = head.next
+            return ix
+        
+        len_list = number_of_nodes(head)
+        m = len_list-n+1
+        current_node = head
+        temp_node = head
+        ix = 1
+        if m == 1:
+            return head.next
 
-        k = num - n
-        if k == 0:
-            head = head.next
-        else:
-            h = head
-            for i in range(k):
-                if i == k-1:
-                    h.next = h.next.next
-                h = h.next
+        while current_node is not None and ix <= m+1:
+            if ix == m-1:
+                temp_node = current_node
+                current_node = current_node.next
+                temp_node.next = None
+            elif ix == m+1:
+                temp_node.next = current_node
+                current_node = current_node.next
+            else:
+                current_node = current_node.next
+            ix += 1
         return head
 
 
