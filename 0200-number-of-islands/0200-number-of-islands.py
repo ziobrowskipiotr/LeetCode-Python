b52b4b634ac9af05a0d8e2637938e6e35fc5e206
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-12-02 00:56:42 +0100

    Time: 328 ms (5.22%), Space: 27.9 MB (9.86%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,27 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         visit_set = set()
-        neighbors = []
         islands = 0
         rows = len(grid)
         columns = len(grid[0])
         directions = [(-1,0), (1,0), (0,-1), (0, 1)]
 
-        def bfs(r, c):
+        def dfs(r, c):
             visit_set.add((r,c))
-            neighbors.append((r,c))
-            while neighbors:
-                row, col = neighbors.pop()
-                for dr, dc in directions:
-                    r, c = row+dr, col+dc
-                    if r in range(rows) and c in range(columns) and (r,c) not in visit_set and grid[r][c] == "1":
-                        visit_set.add((r,c))
-                        neighbors.append((r,c))
+            for dr, dc in directions:
+                row, col = r+dr, c+dc
+                if row in range(rows) and col in range(columns) and (row,col) not in visit_set and grid[row][col] == "1":
+                    dfs(row,col)
 
         for r in range(rows):
             for c in range(columns):
                 if grid[r][c] == "1" and (r,c) not in visit_set:
-                    bfs(r,c)
+                    dfs(r,c)
                     islands += 1
 
         return islands
