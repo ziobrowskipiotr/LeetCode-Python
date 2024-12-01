b123845b41c3f80ba718512287158cb2179b1966
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-12-02 00:48:08 +0100

    Time: 319 ms (6.85%), Space: 25.1 MB (16.56%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,7 +1,7 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         visit_set = set()
-        neighbors = []
+        neighbors = collections.deque()
         islands = 0
         rows = len(grid)
         columns = len(grid[0])
@@ -11,7 +11,7 @@ class Solution:
             visit_set.add((r,c))
             neighbors.append((r,c))
             while neighbors:
-                row, col = neighbors.pop(0)
+                row, col = neighbors.popleft()
                 for dr, dc in directions:
                     r, c = row+dr, col+dc
                     if r in range(rows) and c in range(columns) and (r,c) not in visit_set and grid[r][c] == "1":
