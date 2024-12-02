2a2a793cdf8cc09d83bf645267b9a933a2d2aba2
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-12-02 20:32:16 +0100

    Time: 7 ms (32.93%), Space: 19 MB (6.46%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -5,6 +5,7 @@ class Solution:
         courses_map = {i:[] for i in range(numCourses)}
         courses_set = set()
         result_list = []
+        result_list_set = set()
         for el in prerequisites:
             courses_map[el[0]].append(el[1])
         
@@ -12,12 +13,15 @@ class Solution:
             if course in courses_set:
                 return False
             if courses_map[course] == []:
-                result_list.append(course) if course not in result_list else None
+                if course not in result_list_set:
+                    result_list.append(course)
+                    result_list_set.add(course)
                 return True
             courses_set.add(course)
             for nex in courses_map[course]:
                 if not topological_sort(nex):
                     return False
+            result_list_set.add(course)
             result_list.append(course)
             courses_set.remove(course)
             courses_map[course] = []
@@ -27,4 +31,3 @@ class Solution:
             if not topological_sort(i):
                 return []
         return result_list
-            
