1c485aac6ba3d90db26a61cbcef13f7297ca578d
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-12-02 20:45:18 +0100

    Time: 0 ms (100%), Space: 18.9 MB (5.32%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,19 +1,15 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         courses_map = {i:[] for i in range(numCourses)}
-        courses_set = set()
+        courses_set, result_list_set = set(), set()
         result_list = []
-        result_list_set = set()
         for el in prerequisites:
             courses_map[el[0]].append(el[1])
         
         def topological_sort(course):
             if course in courses_set:
                 return False
-            if courses_map[course] == []:
-                if course not in result_list_set:
-                    result_list.append(course)
-                    result_list_set.add(course)
+            if course in result_list_set:
                 return True
             courses_set.add(course)
             for nex in courses_map[course]:
