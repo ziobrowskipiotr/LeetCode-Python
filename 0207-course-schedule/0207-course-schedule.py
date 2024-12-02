d37ff545d9d428583cb7f74babcc9821b9ae4379
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-12-02 17:55:58 +0100

    Time: 7 ms (48.7%), Space: 19 MB (5.13%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -2,39 +2,26 @@ class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         if not prerequisites:
             return True
-        courses_map = collections.defaultdict(list)
+        courses_map = {i:[] for i in range(numCourses)}
         courses_set = set()
-        self.islands = 0
         for pre in prerequisites:
             courses_map[pre[0]] += [pre[1]]
-            courses_set.add(pre[1])
-
-        for i in range(numCourses):
-            if i not in courses_map:
-                courses_map[i] = []
-            if i not in courses_set:
-                self.islands += 1
-        
-        courses_set = set()
 
         def dfs(course):
-            courses_set.add(course)
-
-            for nei in courses_map[course]:
-                if nei in courses_set:
-                    return False
-                else:
-                    return dfs(nei)
-
-            if len(courses_set)+self.islands == numCourses:
-                return True
-            else:
-                courses_set.remove(course)
+            if course in courses_set:
                 return False
-    
-        for i in courses_map.keys():
-            if dfs(i):
+            if courses_map[course] == []:
                 return True
-            courses_set = set()
-        else:
-            return False
+            
+            courses_set.add(course)
+            for nei in courses_map[course]:
+                if not dfs(nei):
+                    return False
+            courses_set.remove(course)
+            courses_map[course] = []
+            return True
+    
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        return True
