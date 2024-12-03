2b4b019db17348bd776e7cbbbc8b18c174540cab
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-12-03 23:55:15 +0100

    Time: 384 ms (33.89%), Space: 19.3 MB (13.51%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -6,16 +6,16 @@ class Solution:
         for time in times:
             graph_map[time[0]].append((time[2], time[1]))
 
-        result = 0
+        resul = 0
         heapq.heappush(heap, (0, k))
         while heap:
             weight, node = heapq.heappop(heap)
             if node in visit_set:
                 continue
             visit_set.add(node)
-            result = max(result, weight)
+            resul = max(resul, weight)
             for actual_weight, actual_node in graph_map[node]:
                 if actual_node not in visit_set:
                     heapq.heappush(heap, (weight+actual_weight, actual_node))
 
-        return result if len(visit_set) == n else -1 
+        return resul if len(visit_set) == n else -1 
