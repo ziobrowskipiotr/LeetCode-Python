446f698ae3d73ff4f4441096284d7e4a5873243f
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-12-03 23:54:47 +0100

    Time: 388 ms (28.46%), Space: 19.4 MB (10.63%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,17 +1,21 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         heap = []
+        visit_set = set()
         graph_map = collections.defaultdict(list)
         for time in times:
             graph_map[time[0]].append((time[2], time[1]))
 
+        result = 0
         heapq.heappush(heap, (0, k))
         while heap:
             weight, node = heapq.heappop(heap)
+            if node in visit_set:
+                continue
+            visit_set.add(node)
+            result = max(result, weight)
             for actual_weight, actual_node in graph_map[node]:
-                if actual_node == n:
-                    if actual_node == k:
-                        return weight
-                    return weight+actual_weight
-                heapq.heappush(heap, (weight+actual_weight, actual_node))
-        return -1
+                if actual_node not in visit_set:
+                    heapq.heappush(heap, (weight+actual_weight, actual_node))
+
+        return result if len(visit_set) == n else -1 
